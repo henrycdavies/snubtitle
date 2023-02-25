88bada62affbf50e9e7cd953fa75cca513ac6807
Henry Davies <Henry Davies>
2023-02-25 12:53:29 +0000

    Use TypeScript

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -3,17 +3,32 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './bootstrap.js',
+    entry: './src/bootstrap.ts',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bootstrap.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                exclude: /node_module/,
+                use: 'ts-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.js',
+        ]
     },
     mode: 'development',
     plugins: [
         new CopyWebpackPlugin({
             patterns: [
                 {
-                    from: './index.html',
+                    from: './src/index.html',
                     to: './',
                 }
             ]
